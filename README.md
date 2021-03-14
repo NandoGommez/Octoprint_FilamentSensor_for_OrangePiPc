a05fee6e7e012ddf4b9540df977985cd35c6c49b
NandoGommez <79741539+NandoGommez@users.noreply.github.com>
2021-03-13 23:50:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+# OctoPrint-FilamentSensor-ng-OrangePi
+
+I made this because [Octoprint-Filament-Sensor-ng](https://github.com/Red-M/Octoprint-Filament-Sensor-ng) doesn't have support for OrangePi.
+
+[OctoPrint](http://octoprint.org/) plugin that integrates with a filament sensor hooked up to a Orange Pi GPIO pin and allows the filament spool to be changed during a print if the filament runs out.
+
+Initial work based on the [Octoprint-Filament-Sensor-ng](https://github.com/Red-M/Octoprint-Filament-Sensor-ng) plugin by Red-M.
+Initial work based on the [Octoprint-Filament-Reloaded](https://github.com/kontakt/Octoprint-Filament-Reloaded) plugin by kontakt.
+Initial work based on the [Octoprint-Filament](https://github.com/MoonshineSG/Octoprint-Filament) plugin by MoonshineSG.
+
+## Required sensor
+
+Using this plugin requires a filament sensor.
+
+## Features
+
+* Configurable GPIO pin.
+* Debounce noisy sensors.
+* Support norbally open and normally closed sensors.
+* Execution of custom GCODE when out of filament detected.
+* Optionally pause print when out of filament.
+
+## Installation
+
+* Manually using this URL: https://github.com/deadly667/Octoprint-Filament-Sensor-ng/archive/master.zip
+
+## Configuration
+
+After installation, configure the plugin via OctoPrint Settings interface.
+
+The pin being used needs to be entered by name (e.g. PA01, PC07)
+
+## OrangePI OS Configuration
+
+Since we are accessing the GPIO as a non root user we need to configure the OS to allow this. Here's the copy of the library documentation on how to do it:
+
+ If you want to be able to use the library as a non root user, you will need to setup a `UDEV` rule to grant you permissions first. 
+ This can be accomplished as follows: 
+ ```
+ $ sudo usermod -aG gpio <current_user>
+ $ sudo nano /etc/udev/rules.d/99-gpio.rules
+ ```
+ That should add your user to the GPIO group, create a new ``UDEV`` rule, and open it in the Nano text editor. 
+ Enter the following into Nano
+ ```
+   SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:gpio /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'" 
+   SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+ ```   
+ press ``ctrl-x``, ``Y``, and ``ENTER`` to save and close the file. 
+ Finally, reboot and you should be ready to use ``OPi.GPIO`` as a non root user. 
+
+
+## Issues with OPI GPIO library
+
+https://github.com/rm-hull/OPi.GPIO/pull/28 solves the issues with PI GPIO library and the race condition that was preventing normal operations. It is available in release v0.3.4 and up so make sure you have latest OPI GPIO library. To upgrade to latest OPI GPIO library login to your octoprint server as user ``pi`` and execute:
+
+```
+ $ cd OctoPrint/ 
+ $ venv/bin/pip install --upgrade OPi.GPIO
+```
---
+++ octoprint_filament_runout_for_orangepipc/__init__.py
@@ -0,0 +1,173 @@
+# coding=utf-8
+from __future__ import absolute_import
+from flask import jsonify
+
+import octoprint.plugin
+from octoprint.events import Events
+import OPi.GPIO as GPIO
+from time import sleep
+
+
+class FilamentSensorOrangePiPcPlugin(octoprint.plugin.StartupPlugin,
+                             octoprint.plugin.EventHandlerPlugin,
+                             octoprint.plugin.TemplatePlugin,
+                             octoprint.plugin.SettingsPlugin,
+                             octoprint.plugin.RestartNeedingPlugin):
+
+    def initialize(self):
+        self._logger.info("Running OPi.GPIO")
+        GPIO.setwarnings(False)        # Disable GPIO warnings
+        self.FilamentSensorOrangePiPcPlugin_confirmations_tracking = 0
+
+    @property
+    def pin(self):
+        return str(self._settings.get(["pin"]))
+
+    @property
+    def poll_time(self):
+        return int(self._settings.get(["poll_time"]))
+
+    @property
+    def switch(self):
+        return int(self._settings.get(["switch"]))
+
+    @property
+    def confirmations(self):
+        return int(self._settings.get(["confirmations"]))
+
+    @property
+    def debug_mode(self):
+        return int(self._settings.get(["debug_mode"]))
+
+    @property
+    def no_filament_gcode(self):
+        return str(self._settings.get(["no_filament_gcode"])).splitlines()
+
+    @property
+    def pause_print(self):
+        return self._settings.get_boolean(["pause_print"])
+
+    def _setup_sensor(self):
+        if self.sensor_enabled():
+            self._logger.info("Using SUNXI Mode")
+            GPIO.setmode(GPIO.SUNXI)
+            self._logger.info("Filament Sensor active on GPIO Pin [%s]"%self.pin)
+            GPIO.setup(self.pin, GPIO.IN, GPIO.HIGH)
+        else:
+            self._logger.info("Pin not configured, won't work unless configured!")
+
+    def on_after_startup(self):
+        self._logger.info("FilamentSensor-OrangePiPc started")
+        self._setup_sensor()
+
+    def get_settings_defaults(self):
+        return({
+            'pin':'-1',   # Default is no pin
+            'poll_time':250,  # Debounce 250ms
+            'switch':0,    # Normally Open
+            'confirmations':5,# Confirm that we're actually out of filament
+            'no_filament_gcode':'',
+            'debug_mode':0, # Debug off!
+            'pause_print':True
+        })
+    
+    def debug_only_output(self, string):
+        if self.debug_mode==1:
+            self._logger.info(string)
+
+    def on_settings_save(self, data):
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        self._setup_sensor()
+
+    def sensor_enabled(self):
+        return self.pin != '-1'
+
+    def no_filament(self):
+        return GPIO.input(self.pin) != self.switch
+
+    def get_template_configs(self):
+        return [dict(type="settings", custom_bindings=False)]
+
+    def on_event(self, event, payload):
+        # Early abort in case of out ot filament when start printing, as we
+        # can't change with a cold nozzle
+        if event is Events.PRINT_STARTED and self.no_filament():
+            self._logger.info("Printing aborted: no filament detected!")
+            self._printer.cancel_print()
+        # Enable sensor
+        if event in (
+            Events.PRINT_STARTED,
+            Events.PRINT_RESUMED
+        ):
+            self._logger.info("%s: Enabling filament sensor." % (event))
+            if self.sensor_enabled():
+                GPIO.remove_event_detect(self.pin)
+                GPIO.add_event_detect(
+                    self.pin, GPIO.BOTH,
+                    callback=self.sensor_callback,
+                    bouncetime=self.poll_time
+                )
+        # Disable sensor
+        elif event in (
+            Events.PRINT_DONE,
+            Events.PRINT_FAILED,
+            Events.PRINT_CANCELLED,
+            Events.ERROR
+        ):
+            self._logger.info("%s: Disabling filament sensor." % (event))
+            GPIO.remove_event_detect(self.pin)
+
+    @octoprint.plugin.BlueprintPlugin.route("/status", methods=["GET"])
+    def check_status(self):
+        status = "-1"
+        if self.pin != '-1':
+            status = str(self.no_filament())
+        return jsonify( status = status )
+
+    def sensor_callback(self, _):
+        sleep(self.poll_time/1000)
+        self.debug_only_output('Pin: '+str(GPIO.input(self.pin)))
+        if self.no_filament():
+            self.FilamentSensorOrangePiPcPlugin_confirmations_tracking+=1
+            self.debug_only_output('Confirmations: '+str(self.FilamentSensorOrangePiPcPlugin_confirmations_tracking))
+            if self.confirmations<=self.FilamentSensorOrangePiPcPlugin_confirmations_tracking:
+                self._logger.info("Out of filament!")
+                if self.pause_print:
+                    self._logger.info("Pausing print.")
+                    self._printer.pause_print()
+                if self.no_filament_gcode:
+                    self._logger.info("Sending out of filament GCODE")
+                    self._printer.commands(self.no_filament_gcode)
+                self.FilamentSensorOrangePiPcPlugin_confirmations_tracking = 0
+        else:
+            self.FilamentSensorOrangePiPcPlugin_confirmations_tracking = 0
+
+    def get_update_information(self):
+        return dict(
+            octoprint_filament_runout_for_orangepipc=dict(
+                displayName="FilamentSensor OrangePiPc",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="NandoGommez",
+                repo="Octoprint_FilamentSensor_for_OrangePiPc",
+                current=self._plugin_version,
+
+                # update method: pip
+                pip="https://github.com/NandoGommez/Octoprint_FilamentSensor_for_OrangePiPc/archive/master.zip"
+            )
+        )
+
+__plugin_name__ = "FilamentSensor OrangePiPc"
+__plugin_version__ = "2.0.0"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = FilamentSensorOrangePiPcPlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+}
