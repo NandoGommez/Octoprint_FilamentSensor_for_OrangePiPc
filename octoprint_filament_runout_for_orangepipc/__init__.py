2eaebc1251b41e3ffe2ddb7f82efebb2b1b67125
NandoGommez <79741539+NandoGommez@users.noreply.github.com>
2021-03-16 02:11:28 -0300

    Update __init__.py

--- octoprint_filament_runout_for_orangepipc/__init__.py
+++ octoprint_filament_runout_for_orangepipc/__init__.py
@@ -205,7 +205,7 @@ class FilamentSensorOrangePiPcPlugin(octoprint.plugin.StartupPlugin,
         )
 
 __plugin_name__ = "FilamentSensor OrangePiPc"
-__plugin_version__ = "2.1.3"
+__plugin_version__ = "2.1.4"
 __plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
