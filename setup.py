077aafcb55304c73539c04cd67a3f27819119874
NandoGommez <79741539+NandoGommez@users.noreply.github.com>
2021-03-24 14:58:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_filament_runout_for_orangepipc"
 plugin_name = "FilamentSensor-OrangePiPc"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "2.1.25"
+plugin_version = "2.1.26"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
